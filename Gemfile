bd76b4fb0938d861f59e5c69a770656e6f62778f
JenniferDomR <jennifer.dom.r@gmail.com>
2017-05-15 23:34:31 -0500

    Configuración gmail

--- Gemfile
+++ Gemfile
@@ -33,6 +33,8 @@ gem 'jbuilder', '~> 2.5'
 # gem 'bcrypt', '~> 3.1.7'
 gem 'devise', '~> 4.2', '>= 4.2.1'
 
+gem 'paypal-recurring', '~> 1.1'
+
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
 
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -25,6 +25,24 @@ Rails.application.configure do
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
 
+
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { host: 'localhost:3000/' }
+  config.action_mailer.delivery_method = :smtp
+
+    config.action_mailer.smtp_settings =
+    {
+        address:              'smtp.gmail.com',
+        port:                 587,
+        domain:               'thewafflehouses.com',
+        user_name:            'thewafflehouses@gmail.com',
+        password:             'sBU-kHN-JDh-9DQ',
+        authentication:       'plain',
+        enable_starttls_auto: true
+    }
+config.action_mailer.raise_delivery_errors = true
+
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.

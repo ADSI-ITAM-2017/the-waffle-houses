cf412bb87061d3d9fe96e8dd6342332ae4b1d116
royserafin <rodrigocalser@gmail.com>
2017-05-18 02:49:36 -0500

    Búsqueda simple de propiedades

--- app/models/property.rb
+++ app/models/property.rb
@@ -3,4 +3,8 @@ class Property < ApplicationRecord
 
 	has_many :pictures, dependent: :destroy
 
+	def self.search(search)
+  		where("title LIKE ? OR tipo LIKE ? OR colonia LIKE ?", "%#{search}%", "%#{search}%", "%#{search}%") 
+	end
+
 end

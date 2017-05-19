c71eb4e6abffb284e853884ba78efc05d5dd97f7
IanYoav <iyef@hotmail.com>
2017-05-18 23:00:22 -0500

    chema

--- db/schema.rb
+++ db/schema.rb
@@ -63,15 +63,6 @@ ActiveRecord::Schema.define(version: 20170518195713) do
     t.integer  "user_id"
   end
 
-  create_table "searches", force: :cascade do |t|
-    t.string   "keywords"
-    t.decimal  "min_price"
-    t.decimal  "max_price"
-    t.string   "tipo"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
